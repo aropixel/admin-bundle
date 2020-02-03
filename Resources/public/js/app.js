e8ee8b5d89bb739c1a539f7b65991b85febb08d9
Joël Gomez Caballe <joel.gomez@aropixel.com>
2020-02-03 14:10:08 +0100

    Custom clockpicker & datepicker

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -11,7 +11,66 @@
 
 $(function() {
 
-
+    moment.locale('fr', {
+        months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+        monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
+        monthsParseExact : true,
+        weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
+        weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
+        weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+        weekdaysParseExact : true,
+        longDateFormat : {
+            LT : 'HH:mm',
+            LTS : 'HH:mm:ss',
+            L : 'DD/MM/YYYY',
+            LL : 'D MMMM YYYY',
+            LLL : 'D MMMM YYYY HH:mm',
+            LLLL : 'dddd D MMMM YYYY HH:mm'
+        },
+        calendar : {
+            sameDay : '[Aujourd’hui à] LT',
+            nextDay : '[Demain à] LT',
+            nextWeek : 'dddd [à] LT',
+            lastDay : '[Hier à] LT',
+            lastWeek : 'dddd [dernier à] LT',
+            sameElse : 'L'
+        },
+        relativeTime : {
+            future : 'dans %s',
+            past : 'il y a %s',
+            s : 'quelques secondes',
+            m : 'une minute',
+            mm : '%d minutes',
+            h : 'une heure',
+            hh : '%d heures',
+            d : 'un jour',
+            dd : '%d jours',
+            M : 'un mois',
+            MM : '%d mois',
+            y : 'un an',
+            yy : '%d ans'
+        },
+        dayOfMonthOrdinalParse : /\d{1,2}(er|e)/,
+        ordinal : function (number) {
+            return number + (number === 1 ? 'er' : 'e');
+        },
+        meridiemParse : /PD|MD/,
+        isPM : function (input) {
+            return input.charAt(0) === 'M';
+        },
+        // In case the meridiem units are not separated around 12, then implement
+        // this function (look at locale/id.js for an example).
+        // meridiemHour : function (hour, meridiem) {
+        //     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
+        // },
+        meridiem : function (hours, minutes, isLower) {
+            return hours < 12 ? 'PD' : 'MD';
+        },
+        week : {
+            dow : 1, // Monday is the first day of the week.
+            doy : 4  // Used to determine first week of the year.
+        }
+    });
 
 
     // ========================================
@@ -40,32 +99,52 @@ $(function() {
     var $pickadate = $('.pickadate');
     if ($pickadate.length) {
 
-        $pickadate.pickadate({
+        var picker_date = $pickadate.pickadate({
             monthsFull: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
             weekdaysShort: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
             today: 'aujourd\'hui',
             clear: 'effacer',
+            close: 'fermer',
             selectMonths: true,
             selectYears: true,
             labelMonthNext: 'Mois suivant',
             labelMonthPrev: 'Mois précédent',
             format: 'dd/mm/yyyy',
-            formatSubmit: 'yyyy-mm-dd'
+            formatSubmit: 'yyyy-mm-dd',
+            onOpen: function() {
+
+                moment.locale('fr');
+                var selected = $(this)[0].get('select');
+                var dateMoment = selected ? moment(selected.obj) : moment();
+                var dayName = dateMoment.format('dddd')[0].toUpperCase() + dateMoment.format('dddd').slice(1);
+
+                var displayDate = '<div class="picker__date-display"><div class="picker__weekday-display">'+dayName+'</div><div class="picker__month-display"><div>'+dateMoment.format('MMM')+'</div></div><div class="picker__day-display"><div>'+dateMoment.format('D')+'</div></div><div class="picker__year-display"><div>'+dateMoment.format('YYYY')+'</div></div></div>';
+                $(this)[0].$root.find( '.picker__wrap .picker__date-display' ).remove();
+                $(this)[0].$root.find( '.picker__wrap' ).prepend(displayDate);
+                $(this)[0].render();
+
+            }
         });
 
     }
 
 
+
     var $pickatime = $('.pickatime');
     if ($pickatime.length) {
 
-        $pickatime.pickatime({
-            clear: 'Effacer',
-            format: 'HH:i',
-            formatLabel: 'HH:i',
-            formatSubmit: 'H:i',
-            interval: 15,
-            hiddenName: true
+        // $pickatime.pickatime({
+        //     clear: 'Effacer',
+        //     format: 'HH:i',
+        //     formatLabel: 'HH:i',
+        //     formatSubmit: 'H:i',
+        //     interval: 15,
+        //     hiddenName: true
+        // });
+        $pickatime.clockpicker({
+            amOrPm: false,
+            cleartext: 'Effacer',
+            donetext: 'Valider'
         });
 
     }
