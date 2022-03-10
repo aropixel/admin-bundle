2c716ea97772920c2d5534c56163413d0eb05cea
Cecile Salon <cecile.salon@aropixel.com>
2022-03-10 09:00:49 +0100

    Ajout de UserRepositoryInterface

--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -12,7 +12,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
---
+++ Repository/UserRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Aropixel\AdminBundle\Repository;
+
+
+interface UserRepositoryInterface
+{
+
+}
