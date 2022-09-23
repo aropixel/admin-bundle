a0785d90a17a916256357df1d81b433b2d86aade
Antoine Leclaire <104761460+Ate-lcr@users.noreply.github.com>
2022-09-23 10:04:51 +0200

    Move DownloadAction in a specific controller to generate proper URL

---
+++ Controller/DownloadController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Aropixel\AdminBundle\Controller;
+
+use Aropixel\AdminBundle\Entity\File;
+use Aropixel\AdminBundle\Resolver\PathResolverInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DownloadController extends AbstractController
+{
+    /** @var PathResolverInterface */
+    private $pathResolver;
+
+
+    /**
+     * @param PathResolverInterface $pathResolver
+     */
+    public function __construct(PathResolverInterface $pathResolver)
+    {
+        $this->pathResolver = $pathResolver;
+    }
+
+    /**
+     * Upload a File.
+     *
+     * @Route("/download/{id}/{filename}", name="file_download", methods={"GET"})
+     */
+    public function downloadAction(File $file)
+    {
+        $path = $this->pathResolver->getAbsolutePath(File::UPLOAD_DIR, $file->getFilename());
+        return $this->file($path, $file->getRewritedFileName());
+    }
+
+
+}
