7f1e5223f3de1b512d83a01e8f8c294dd464b42e
Joël Gomez Caballe <joel.gomez@aropixel.com>
2023-10-13 14:55:28 +0200

    Datatable en francais

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -292,9 +292,11 @@ $(function() {
         language: {
             search: '<span>Filtrer :</span> _INPUT_',
             lengthMenu: '<span>Nombre par page:</span> _MENU_',
-            sInfo: "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
-            sInfoEmpty: "Affichage de l'&eacute;l&eacute;ment 0 &agrave; 0 sur 0 &eacute;l&eacute;ment",
-            sEmptyTable: "Aucun &eacute;l&eacute;ment &agrave; afficher",
+            emptyTable: "Aucun &eacute;l&eacute;ment &agrave; afficher",
+            info: "Affichage de l'&eacute;l&eacute;ment _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+            infoEmpty: "Aucun &eacute;l&eacute;ment &agrave; afficher",
+            infoFiltered: "(sur _MAX_ &eacute;l&eacute;ments disponibles)",
+            zeroRecords: "Aucun &eacute;l&eacute;ment &agrave; afficher",
             paginate: { 'first': 'Premier', 'last': 'Dernier', 'next': '&rarr;', 'previous': '&larr;' }
         },
         drawCallback: function () {
